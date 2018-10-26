c45fbf30ad624a60ac8529827e88562b8cd6229d
Huiyeon Kim <gnldus59112@gmail.com>
2018-10-26 15:06:15 +0800

    Styling the split screen

--- src/components/Half/Half.js
+++ src/components/Half/Half.js
@@ -7,20 +7,17 @@ class Half extends Component {
         if(this.props.type === "SAT"){
             return (
                 <div className="Half red">
-                    <div className="half-cover">
-                        <div className="img-div">
-                            <img src={require("../../img/SAT.png")} alt="" srcset="" className="Half-img"/>
-                        </div>
+                    <div className="img-div">
+                        <img src={require("../../img/SAT.png")} alt="" srcset="" className="Half-img"/>
                     </div>
+                    <p className="description">The SAT is a standardized test meant to show schools how prepared you are for college by measuring key skills like reading comprehension, computational ability, and clarity of expression. Because so many students take the test, it also provides schools with data about how you compare to your peers nationwide.</p>
                 </div>
             );
         }else{
             return (
                 <div className="Half blue">
-                    <div className="half-cover">
-                        <div className="img-div">
-                            <img src={require("../../img/ACT.png")} alt="" srcset="" className="Half-img"/>
-                        </div>
+                    <div className="img-div">
+                        <img src={require("../../img/ACT.png")} alt="" srcset="" className="Half-img"/>
                     </div>
                 </div>
             );
